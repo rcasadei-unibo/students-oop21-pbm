bf84f281778bc8482914d6a36708075c15ed786b
Song Zhaohui <zhaohui.song@studio.unibo.it>
2022-07-18 17:36:36 +0200

    minor fix

--- src/main/java/main/application/App.java
+++ src/main/java/main/application/App.java
@@ -32,4 +32,8 @@ public class App extends Application {
  public static void main(String[] args) {
         launch(args);
     }
+ 
+public Object getGreeting() {
+	return "something";
+}
 }
--- src/test/java/main/application/AppTest.java
+++ src/test/java/main/application/AppTest.java
@@ -12,6 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
-       // assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+       assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
