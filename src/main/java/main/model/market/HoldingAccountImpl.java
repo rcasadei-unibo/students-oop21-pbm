4ce1ad9f86090a2bb1ed6208f078d41074da40df
Song Zhaohui <zhaohui.song@studio.unibo.it>
2022-07-25 11:55:04 +0200

    add holding account

--- src/main/java/main/model/market/HoldingAccountImpl.java
+++ src/main/java/main/model/market/HoldingAccountImpl.java
@@ -1,35 +1,44 @@
 package main.model.market;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.google.common.base.Optional;
+
 public class HoldingAccountImpl implements HoldingAccount {
 
 	private Map<String, Double> holdings;
+	private EquityPool equityPool;
 
-	public HoldingAccountImpl(final Map<String, Double> holdings) {
+	public HoldingAccountImpl(final Map<String, Double> holdings, final EquityPool ep) {
 		super();
 		this.holdings = holdings;
+		equityPool = ep;
 	}
 
-	public HoldingAccountImpl() {
-		holdings = new HashMap<>();
+	public HoldingAccountImpl(final EquityPool ep) {
+		this(new HashMap<>(), ep);
 	}
 
 	@Override
 	public Set<String> getHoldingSymbols() {
 		return holdings.entrySet().stream().map(x -> x.getKey()).collect(Collectors.toSet());
 	}
-
 	@Override
-	public double getTotalValue() {
-		return holdings.entrySet().stream().mapToDouble(x -> EquityPool
-				.getEquityPrice(x.getKey()) * x.getValue())
+	public double getTotalValue(){
+		/**
+		 * the x.getKey().get() might be absent. 
+		 * So I need to decide whether to tell the user to wait or 
+		 * just show an error.
+		 */
+		return holdings.entrySet().stream().mapToDouble(x -> equityPool
+				.getEquityPrice(x.getKey()).get() * x.getValue())
 				.sum();
 	}
-
+	
 	@Override
 	public void updateHoldingsForBuying(final Order order) {
 		final String key = order.getEquity().getSymbol();
@@ -51,7 +60,7 @@ public class HoldingAccountImpl implements HoldingAccount {
 
 	@Override
 	public boolean hasEnoughShares(final Order order) {
-		return holdings.containsKey(order.getEquity().getSymbol());
+		return holdings.get(order.getEquity().getSymbol()) >= order.getShares();
 	}
 
 }
