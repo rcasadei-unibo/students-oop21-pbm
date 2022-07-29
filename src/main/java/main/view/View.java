0a1572822a214e22190df0ac5f81042107d6caef
Song Zhaohui <zhaohui.song@studio.unibo.it>
2022-07-29 12:32:50 +0200

    add GUI factory, it's still in construction..

---
+++ src/main/java/main/view/View.java
@@ -0,0 +1,15 @@
+package main.view;
+
+/**
+ * This interface models an independent implementation of 
+ * GUI.
+ *
+ */
+public interface View {
+	
+	/**
+	 * Display contents.
+	 * @param args the params from main
+	 */
+	void show(String[] args);
+}
