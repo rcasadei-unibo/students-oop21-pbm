cc56be86d43d8aa6e88661ae2196274cdfa3678a
Paolo Pietrelli <paolo.pietrelli@studio.unibo.it>
2022-08-11 19:05:58 +0200

    testing operation json

--- src/main/java/main/charts/OperationJSONUtente.java
+++ src/main/java/main/charts/OperationJSONUtente.java
@@ -3,6 +3,7 @@ package main.charts;
 import java.io.Reader;
 import java.math.BigDecimal;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Map;
@@ -12,10 +13,59 @@ import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
 
 public class OperationJSONUtente {
-    String username;
+    private String username;
+    private String password;
     double totalAmount;
+    /*
+    public OperationJSONUtente(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }*/
     
-    boolean userExist (String username, String password) {
+    //this method check if the username and password exist in the JSON file utente 
+    boolean userPasswordCheck (String username, String password) {
+        
+        System.out.println("what's up bro ");
+        
+        try {
+            // create a reader
+            String fileName = "utente.json";
+            Path pathToFile = Paths.get(fileName);
+            System.out.println(pathToFile.toAbsolutePath());
+            Reader reader = Files.newBufferedReader(pathToFile.toAbsolutePath());
+
+            // create parser
+            JsonObject parser = (JsonObject) Jsoner.deserialize(reader);
+
+            // read user
+            JsonArray users = (JsonArray) parser.get("users");
+            Iterator<Object> iterator = users.iterator();
+            System.out.println("hey man ");
+            while(iterator.hasNext()) {
+                JsonObject user = (JsonObject) iterator.next();
+                System.out.println("ciao ");
+                // check username and password
+                if(((String)user.get("username") == username) && ((String)user.get("budget") == password)) {
+                    //close reader
+                    reader.close();
+                    return true;
+                }
+            }
+
+            //close reader
+            reader.close();
+            return false;
+
+        } catch (Exception ex) {
+            System.out.println("oh shit! ");
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    
+    
+    //search user
+    boolean userExist (String username) {
         
         try {
             // create a reader
@@ -29,7 +79,8 @@ public class OperationJSONUtente {
             Iterator<Object> iterator = users.iterator();
             while(iterator.hasNext()) {
                 JsonObject user = (JsonObject) iterator.next();
-                if(((String)user.get("username") == username) && ((String)user.get("budget") == password)) {
+                // check username and password
+                if(((String)user.get("username") == username)) {
                     //close reader
                     reader.close();
                     return true;
@@ -41,9 +92,13 @@ public class OperationJSONUtente {
             return false;
 
         } catch (Exception ex) {
+            System.out.println("oh shit! ");
             ex.printStackTrace();
             return false;
         }
     }
+    
+    
+    
 
 }
