6e8ed013f08b552741f318c8f252a91d4e698f86
Paolo Pietrelli <paolo.pietrelli@studio.unibo.it>
2022-08-19 02:43:56 +0200

    new class for the transaction

---
+++ src/main/java/main/charts/TransactionJson.java
@@ -0,0 +1,24 @@
+package main.charts;
+
+public class TransactionJson {
+    
+    String nameTransaction, date, time, currency;
+    double amount;
+    
+    TransactionJson(String nameTransaction, String date, String time, double amount){
+        this.amount = amount;
+        this.nameTransaction = nameTransaction;
+        this.date = date;
+        this.time = time;
+    }
+    
+    TransactionJson(String nameTransaction, String date, String time, double amount, String currency){
+        this.amount = amount;
+        this.nameTransaction = nameTransaction;
+        this.date = date;
+        this.time = time;
+        this.currency = currency;
+    }
+    
+
+}
